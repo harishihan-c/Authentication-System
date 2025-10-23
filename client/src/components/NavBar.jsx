bf3cf1390521ab0f8a1a61d5551f9c800c1bb47a
harishihan-c <harishihan@outlook.com>
2025-10-24 00:52:46 +0530

    feat: Add NavBar component

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import { assets } from "../assets/assets.js";
+
+const NavBar = () => {
+  return (
+    <div className="w-full flex justify-between items-center px-4 py-2 sm:p-6 sm:px-24 absolute top-0">
+      <img src={assets.logo} alt="" className="w-24 sm:w-32" />
+      <button className="flex items-center gap-3 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all">
+        Login <img src={assets.arrow_icon} alt="" />
+      </button>
+    </div>
+  );
+};
+
+export default NavBar;
