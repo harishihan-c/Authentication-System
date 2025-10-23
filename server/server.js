d5865911370b3903e3db32e46c5498213beb356a
harishihan-c <harishihan@outlook.com>
2025-10-23 15:15:03 +0530

    feat: add user router to server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import ConnectDB from "./config/database.js";
 import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
   res.send("Api Working");
 });
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
