6eaf164dc9c24040f5fcf201d664c1e8d07ff58e
Harishihan Chandrakumar <harishihan@outlook.com>
2025-11-09 19:49:46 +0530

    Update allowedOrigins for CORS configuration for deplyment

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import userRouter from "./routes/userRouter.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://authentication-system-frontend-topaz.vercel.app/']
 
 ConnectDB();
 
